a7d7a077f02f2879d30dd959318001adb74323a9
Max <max.edison@yahoo.com>
2021-09-19 13:53:07 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    const writtenCards = [];
+    for(let a=0; a < names.length; a++){
+        writtenCards.push(`Thank you, ${names[a]}, for the wonderful ${event} gift!`);}
+        return writtenCards;
+}  
+//returns an array of thank you messages for each name provided to the function
+
+function countDown(start){
+    while (start >= 0){
+        console.log(start);
+        start--
+    }
+}
+//invokes console.log once for each number, counting down from the number provided to zero
+//logs each number when counting down, starting from the number provided
